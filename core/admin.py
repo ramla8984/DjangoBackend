65d87b415eabfc28f0b4d64cfe06114da622070a
Ramla <ramlamohd8984@gmail.com>
2025-02-07 17:02:07 +0300

    fix requiremnt

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . models import *
 
+admin.site.register(Student)
+admin.site.register(Supervisor)
+admin.site.register(Admin)
+admin.site.register(FieldPlacement)
 # Register your models here.
